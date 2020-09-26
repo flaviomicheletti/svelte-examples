afd042ad11f186df44576c7ed3f68adecce0c92a
Flávio Micheletti <flavio.micheletti@midway.com.br>
2020-09-26 18:57:38 -0300

    06 bindings

---
+++ 06bindings/src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+  import Home from './Home.svelte';
+  import CheckboxInputs from './CheckboxInputs.svelte';
+  import NumericInputs from './NumericInputs.svelte';
+
+  export let menu = 1;
+</script>
+
+<style>
+  ul#menu li {
+    display: inline;
+  }
+</style>
+
+<ul id="menu">
+  <li><a href="/" on:click|preventDefault={() => (menu = 1)}>Home</a></li>
+  |
+  <li><a href="/" on:click|preventDefault={() => (menu = 2)}>Checkbox Inputs</a></li>
+  |
+  <li><a href="/" on:click|preventDefault={() => (menu = 3)}>Numeric Inputs</a></li>
+</ul>
+
+{#if menu === 1}
+  <Home />
+{:else if menu === 2}
+  <CheckboxInputs />
+{:else if menu === 3}
+  <NumericInputs />
+{:else}
+  <h1>Page Not Found</h1>
+{/if}
