3e3dc9aae1e46bf892192a02e6cf7bdf245ffb68
Flávio Micheletti <flavio.micheletti@midway.com.br>
2020-09-26 16:37:02 -0300

    02 reactivity

---
+++ 02reactivity/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Exemplo1 from './ReactiveAssignments.svelte';
+  import Exemplo2 from './ReactiveDeclarations.svelte';
+  import Exemplo3 from './ReactiveStatements.svelte';
+</script>
+
+<main>
+  <h1>Exemplos em Svelte</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte
+    apps.
+  </p>
+
+  <Exemplo1 />
+  <Exemplo2 />
+  <Exemplo3 />
+</main>
