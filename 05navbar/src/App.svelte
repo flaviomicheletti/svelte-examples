a71a8305dbca46d7bd9be81819b5b8833a49fbd5
Flávio Micheletti <flavio.micheletti@midway.com.br>
2020-09-26 18:41:12 -0300

    05 a simple nav bar

---
+++ 05navbar/src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Beranda from './Beranda.svelte';
+  import Profile from './Profile.svelte';
+
+  export let menu = 1;
+</script>
+
+<style>
+  ul#menu li {
+    display: inline;
+  }
+</style>
+
+<ul id="menu">
+  <li><a href="/" on:click|preventDefault={() => (menu = 1)}>Beranda</a></li>
+  |
+  <li><a href="/" on:click|preventDefault={() => (menu = 2)}>Profile</a></li>
+</ul>
+
+{#if menu === 1}
+  <Beranda />
+{:else if menu === 2}
+  <Profile />
+{:else}
+  <h1>Page Not Found</h1>
+{/if}
